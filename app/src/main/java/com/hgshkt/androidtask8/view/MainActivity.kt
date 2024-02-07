0e59cd90a3a1c89f8177b1889961c2030b4aeda7
hgshkt <nichtojestvozxc@gmail.com>
2024-02-07 22:23:30 +0200

    fragments added to main activity class

--- app/src/main/java/com/hgshkt/androidtask8/view/MainActivity.kt
+++ app/src/main/java/com/hgshkt/androidtask8/view/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.hgshkt.androidtask8.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hgshkt.androidtask8.R
+import com.hgshkt.androidtask8.view.fragments.create.CreateTaskFragment
+import com.hgshkt.androidtask8.view.fragments.list.TaskListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var taskListFragment: TaskListFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+
+        taskListFragment.addButtonClick = {
+            val createFragment = CreateTaskFragment()
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, createFragment)
+                .addToBackStack(createFragment.javaClass.name)
+                .commit()
+        }
+    }
+
+    private fun init() {
+        taskListFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as TaskListFragment
     }
 }
