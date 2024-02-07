c81868162f5c5d3c044b9e75cbd5f5021bb1b427
hgshkt <nichtojestvozxc@gmail.com>
2024-02-07 06:16:34 +0200

    main activity replaced to view package

--- app/src/main/java/com/hgshkt/androidtask8/view/MainActivity.kt
+++ app/src/main/java/com/hgshkt/androidtask8/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hgshkt.androidtask8
+package com.hgshkt.androidtask8.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hgshkt.androidtask8.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
